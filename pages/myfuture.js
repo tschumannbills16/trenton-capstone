7f36dd70fb6d998c3181612337b2370cc083aa8d
Trenton Schumann <trentonschumann@outlook.com>
2022-06-01 10:28:17 -0700

    final comit presentation

--- pages/myfuture.js
+++ pages/myfuture.js
@@ -28,18 +28,19 @@ export default function MyFuture() {
           </div>
 
           <div className={styles.texttwo}>
-            I don&#39;t want to get to fancy with my lifestyle, I want to be
-            able to afford anything without going into bad debt, I want to save
-            myself from buying expensive and unnecessary stuff, so I can work
-            that towards investments and savings instead. Researching and
-            learning about different investments, have been a great interest to
-            me, I plan to read more books and learn new skills so I can use It
-            for my job or my personal life. I want a career that will allow me
-            to work at home or at a office, so I can do tasks in different
-            environments and have an fresh mind. As an active person I want to
-            start eating healthier foods, so I can be motivated to power through
-            the day. Eating junk food hasn&#39;t been a issue for me now but it
-            will effect me. later in my adulthood when i&#39;m not as strong.
+            I don&#39;t want to get to fancy with my lifestyle&#46; I want to be
+            able to afford anything without going into bad debt&#44; I want to
+            save myself from buying expensive and unnecessary stuff&#44; so I
+            can work that towards investments and savings instead&#46;
+            Researching and learning about different investments, have been a
+            great interest to me, I plan to read more books and learn new skills
+            so I can use It for my job or my personal life. I want a career that
+            will allow me to work at home or at a office, so I can do tasks in
+            different environments and have an fresh mind. As an active person I
+            want to start eating healthier foods, so I can be motivated to power
+            through the day. Eating junk food hasn&#39;t been a issue for me now
+            but it will effect me. later in my adulthood when i&#39;m not as
+            strong.
           </div>
         </main>
       </div>
--- pages/projects.js
+++ pages/projects.js
@@ -8,11 +8,11 @@ export default function MyFuture() {
       <div>
         <main>
           <div className={styles.aboutme}>
-            I am very creative individual and love to take things and put them
-            together. This website is an example of how I was able to take an
-            idea and use a web programming tool like javascript to develop a
-            functioning website. As my programming skills evolve so will the
-            quality of my websites.
+            I am very creative individual, and love to take things and put them
+            together&#46; This website is an example of how I was able to take
+            an idea&#44; and use a web programming tool like javascript to
+            develop a functioning website&#46; As my programming skills evolve
+            so will the quality of my website&#46;
           </div>
           <div className={styles.container}></div>
         </main>
