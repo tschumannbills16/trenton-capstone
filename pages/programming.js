105c444b6781486cc7676e0e6e6c51aee0a3a22c
Trenton Schumann <trentonschumann@outlook.com>
2022-06-13 09:51:37 -0700

    background white

--- pages/programming.js
+++ pages/programming.js
@@ -7,22 +7,7 @@ export default function Programming() {
       <HeaderBar headerText="Programming " linkTo="/" />
 
       <main className={styles.p2main}>
-        <h1 className={styles.title}>
-          Programming&#47;coding is one of the things I am interested in lately,
-          it also ties in really well with graphic design, which also piques my
-          interest. The cool thing about programming is there are many languages
-          which can be used to achieve a desired result. I really like using
-          java script because it provides complete control over how a website
-          will function. In fact I used java script to build this website. Fun
-          Fact: most popular websites like Netflix and Microsopft use
-          javascript. There are also many pre-formatted website builders like
-          Wix which can be used by non programmers to build fast and easy
-          websites, but they also have limited functionality and freedom to
-          format. Personally I prefer to code my own website, the possibilities
-          are endless and its quite fun to mess around with the code&#46;
-          Another reason&#44; I like coding is I get to spend quality time and
-          connect with my older brother Turner who also likes to code.
-        </h1>
+        <h1 className={styles.title}></h1>
         <h1></h1>
       </main>
     </div>
